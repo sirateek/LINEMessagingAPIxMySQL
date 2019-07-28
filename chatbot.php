c6351d94e3d38f12a500af753ddb24b281a34880
MaiYaRap KunG <siratee@windowslive.com>
2019-07-28 22:53:34 +0700

    LineMessagingAPIxMySQL

---
+++ chatbot.php
@@ -0,0 +1,63 @@
+<?php
+	$LINEData = file_get_contents('php://input');
+  $jsonData = json_decode($LINEData,true);
+
+  $replyToken = $jsonData["events"][0]["replyToken"];
+  $userID = $jsonData["events"][0]["source"]["userId"];
+  $text = $jsonData["events"][0]["message"]["text"];
+  $timestamp = $jsonData["events"][0]["timestamp"];
+
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "LINE";
+  $mysql = new mysqli($servername, $username, $password, $dbname);
+  mysqli_set_charset($mysql, "utf8");
+
+  if ($mysql->connect_error){
+  $errorcode = $mysql->connect_error;
+  print("MySQL(Connection)> ".$errorcode);
+  }
+
+  function sendMessage($replyJson, $sendInfo){
+          $ch = curl_init($sendInfo["URL"]);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+          curl_setopt($ch, CURLOPT_POST, true);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+              'Content-Type: application/json',
+              'Authorization: Bearer ' . $sendInfo["AccessToken"])
+              );
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
+          $result = curl_exec($ch);
+          curl_close($ch);
+    return $result;
+  }
+
+  $mysql->query("INSERT INTO `LOG`(`UserID`, `Text`, `Timestamp`) VALUES ('$userID','$text','$timestamp')");
+
+  $getUser = $mysql->query("SELECT * FROM `Customer` WHERE `UserID`='$userID'");
+  $getuserNum = $getUser->num_rows;
+  $replyText["type"] = "text";
+  if ($getuserNum == "0"){
+    $replyText["text"] = "สวัสดีคับบบบ";
+  } else {
+    while($row = $getUser->fetch_assoc()){
+      $Name = $row['Name'];
+      $Surname = $row['Surname'];
+      $CustomerID = $row['CustomerID'];
+    }
+    $replyText["text"] = "สวัสดีคุณ $Name $Surname (#$CustomerID)";
+  }
+
+  $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
+  $lineData['AccessToken'] = "hHP4XSADzTbtcGybvA4C4y/s62Ti5cQIR1DEcVw1MSsuKeJUcnfyBF6lkBjphWtV5u2df/z7t+B7XLUm5zUM3TdT3vAymGOeYHqWuIHiDjQ5ngasmbM7DAHz5KcK4H8/z2QU71y9mkGhtxPL6ZxX/QdB04t89/1O/w1cDnyilFU=";
+
+  $replyJson["replyToken"] = $replyToken;
+  $replyJson["messages"][0] = $replyText;
+
+  $encodeJson = json_encode($replyJson);
+
+  $results = sendMessage($encodeJson,$lineData);
+  echo $results;
+  http_response_code(200);
