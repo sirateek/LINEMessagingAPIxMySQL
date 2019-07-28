d3f2d577952ed19e4d4a03c16487e6b6c40fcaa3
Siratee K <siratee@windowslive.com>
2019-07-28 22:54:37 +0700

    Update chatbot.php

--- chatbot.php
+++ chatbot.php
@@ -1,5 +1,5 @@
 <?php
-	$LINEData = file_get_contents('php://input');
+  $LINEData = file_get_contents('php://input');
   $jsonData = json_decode($LINEData,true);
 
   $replyToken = $jsonData["events"][0]["replyToken"];
